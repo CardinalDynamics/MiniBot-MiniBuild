13ecac560a4cede3605666cedc849094800593f0
GoldenPhoenix42 <92963581+GoldenPhoenix42@users.noreply.github.com>
2021-12-13 15:25:28 -0800

    Update Robot.java

--- Drift bot/src/main/java/frc/robot/Robot.java
+++ Drift bot/src/main/java/frc/robot/Robot.java
@@ -16,100 +16,118 @@ import edu.wpi.first.wpilibj.PWMTalonSRX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot
-{
+public class Robot extends TimedRobot {
   // Motor Controllers
   private final PWMVictorSPX m_frontLeft = new PWMVictorSPX(0);
   private final PWMVictorSPX m_backLeft = new PWMVictorSPX(1);
   private final PWMVictorSPX m_frontRight = new PWMVictorSPX(2);
   private final PWMVictorSPX m_backRight = new PWMVictorSPX(3);
   private final XboxController m_controller = new XboxController(0);
-  private DifferentialDrive m_myRobot;
+  private DifferentialDrive m_drive1;
+
   SpeedControllerGroup m_left = new SpeedControllerGroup(m_frontLeft, m_backLeft);
   SpeedControllerGroup m_right = new SpeedControllerGroup(m_frontRight, m_backRight);
- 
+  private boolean drive;
+
   /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
   @Override
   public void robotInit() {
-  
+
     // Tank drive (left motor, right motor)
-    m_myRobot  = new DifferentialDrive(m_left, m_right);
+    m_drive1 = new DifferentialDrive(m_left, m_right);
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+  }
 
   /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
    */
   @Override
   public void autonomousInit() {
-  
+
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-   
+
   }
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() 
-  {
-    //m_robotDrive.arcadeDrive(m_controller.getY(GenericHID.Hand.kLeft), m_controller.getX(GenericHID.Hand.kLeft));
+  public void teleopInit() {
+    // m_robotDrive.arcadeDrive(m_controller.getY(GenericHID.Hand.kLeft),
+    // m_controller.getX(GenericHID.Hand.kLeft));
   }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_controller.getY(GenericHID.Hand.kLeft), m_controller.getY(GenericHID.Hand.kRight));
-    //m_robotDrive.arcadeDrive(m_controller.getY(GenericHID.Hand.kLeft), m_controller.getX(GenericHID.Hand.kLeft));
+    // m_drive1.tankDrive(m_controller.getY(GenericHID.Hand.kLeft),
+    // m_controller.getY(GenericHID.Hand.kRight));
+    // m_robotDrive.arcadeDrive(m_controller.getY(GenericHID.Hand.kLeft),
+    // m_controller.getX(GenericHID.Hand.kLeft));
   }
 
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   /** This function is called once when test mode is enabled. */
   @Override
-  public void testInit() 
-  {
-    //setSafetyEnabled( False);
+  public void testInit() {
+    // setSafetyEnabled( False);
+    drive = true;
   }
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() 
-  {
-    m_myRobot.tankDrive(m_controller.getY(GenericHID.Hand.kLeft), m_controller.getY(GenericHID.Hand.kRight));
-    //m_robotDrive.arcadeDrive(m_controller.getY(GenericHID.Hand.kLeft), m_controller.getX(GenericHID.Hand.kLeft));
+  public void testPeriodic() {
+
+    if (m_controller.getAButtonPressed()) {
+      drive = !drive;
+    }
+
+    if (drive == true) {
+      m_drive1.tankDrive(m_controller.getY(GenericHID.Hand.kLeft), m_controller.getY(GenericHID.Hand.kRight));
+    } else {
+      m_drive1.arcadeDrive(m_controller.getY(GenericHID.Hand.kLeft), m_controller.getX(GenericHID.Hand.kLeft));
+    }
   }
 
 }
